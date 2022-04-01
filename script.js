b9b1f2fde795fb92c3c43e4a152ee6a784087846
DarthVader19 <kimwexler19@gmail.com>
2022-04-01 13:16:19 +0530

    version added

--- script.js
+++ script.js
@@ -8,17 +8,55 @@ const list=document.querySelector('.list')
 const win=document.querySelector('.win');
 const su=document.querySelector('.scoreuser');
 const sb=document.querySelector('.scorebot');
+const ver=document.querySelector('.version');
+const checkbox=document.querySelector('.sw');
+let  choice='N';
 // imgUser.src="scissor.png";
 
+
+checkbox.addEventListener('change',function(){
+    if(checkbox.checked)
+    {
+        alert("Are you sure you want  to continue");
+       choice= prompt("Hi,solve this puzzle to access the content\nValue of arcsin(2)\nEnter the choice below\na). 1\nb). 0\nc). Doesn't Exist",'eg. a ')=='c'?choice='A':call();
+       function call(){
+           alert("sorry  wrong answer");
+           choice='N';
+
+       }
+        
+        console.log(choice);
+        ver.textContent="Adult Version";
+        const game2=new Game();
+        
+    // console.log("checked");
+
+    }
+
+    else
+    {
+        choice='N';
+        console.log(choice);
+        ver.textContent="Normal Version";
+        const game2=new Game();
+        console.log('not checked');
+        
+    }
+    
+    
+    
+})
 class Game{
     #bot;
-    #str={0:'rock',1:'paper',2:'scissor'};
+    #str1={0:'therock11',1:'thepaper',2:'thescissor'};
+    #str=choice==='A'?this.#str1:{0:'rock',1:'paper',2:'scissor'};
     user;
     count=0;
     #scoreUser=0;
     #scoreBot=0;
     constructor(){
         this._userInput();
+        console.log(choice);
         
     }
     _rand(){
@@ -28,6 +66,7 @@ class Game{
     _botInput(){
         
         this.#bot=this._rand();
+        
         
          imgBot.src=`${this.#str[this.#bot]}.png`;
         //  console.log(this.#bot);
